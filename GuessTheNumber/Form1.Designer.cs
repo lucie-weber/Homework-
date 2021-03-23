d991e7e77d78342f51c2d805067f25eb25cbbd9b
Lucie Weber <lucy.petrosyan2003@gmail.com>
2021-03-23 21:51:06 +0400

    some changes

---
+++ GuessTheNumber/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace GuessTheNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtplayernum = new System.Windows.Forms.TextBox();
+            this.lblai = new System.Windows.Forms.Label();
+            this.lbAiSpeech = new System.Windows.Forms.Label();
+            this.lblplayer = new System.Windows.Forms.Label();
+            this.btnbigger = new System.Windows.Forms.Button();
+            this.btnsmaller = new System.Windows.Forms.Button();
+            this.btnrightnumber = new System.Windows.Forms.Button();
+            this.btncheck = new System.Windows.Forms.Button();
+            this.lblAiQuestion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(354, 197);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(116, 41);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtplayernum
+            // 
+            this.txtplayernum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtplayernum.Location = new System.Drawing.Point(382, 72);
+            this.txtplayernum.Name = "txtplayernum";
+            this.txtplayernum.Size = new System.Drawing.Size(88, 26);
+            this.txtplayernum.TabIndex = 1;
+            // 
+            // lblai
+            // 
+            this.lblai.AutoSize = true;
+            this.lblai.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblai.Location = new System.Drawing.Point(36, 10);
+            this.lblai.Name = "lblai";
+            this.lblai.Size = new System.Drawing.Size(42, 31);
+            this.lblai.TabIndex = 2;
+            this.lblai.Text = "AI";
+            // 
+            // lbAiSpeech
+            // 
+            this.lbAiSpeech.AutoSize = true;
+            this.lbAiSpeech.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAiSpeech.Location = new System.Drawing.Point(12, 55);
+            this.lbAiSpeech.Name = "lbAiSpeech";
+            this.lbAiSpeech.Size = new System.Drawing.Size(0, 20);
+            this.lbAiSpeech.TabIndex = 2;
+            // 
+            // lblplayer
+            // 
+            this.lblplayer.AutoSize = true;
+            this.lblplayer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblplayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblplayer.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblplayer.Location = new System.Drawing.Point(353, 10);
+            this.lblplayer.Name = "lblplayer";
+            this.lblplayer.Size = new System.Drawing.Size(94, 31);
+            this.lblplayer.TabIndex = 2;
+            this.lblplayer.Text = "player";
+            // 
+            // btnbigger
+            // 
+            this.btnbigger.Location = new System.Drawing.Point(12, 131);
+            this.btnbigger.Name = "btnbigger";
+            this.btnbigger.Size = new System.Drawing.Size(74, 42);
+            this.btnbigger.TabIndex = 0;
+            this.btnbigger.Text = "Bigger";
+            this.btnbigger.UseVisualStyleBackColor = true;
+            this.btnbigger.Click += new System.EventHandler(this.btnbigger_Click);
+            // 
+            // btnsmaller
+            // 
+            this.btnsmaller.Location = new System.Drawing.Point(92, 131);
+            this.btnsmaller.Name = "btnsmaller";
+            this.btnsmaller.Size = new System.Drawing.Size(74, 42);
+            this.btnsmaller.TabIndex = 0;
+            this.btnsmaller.Text = "SMALLER";
+            this.btnsmaller.UseVisualStyleBackColor = true;
+            this.btnsmaller.Click += new System.EventHandler(this.btnsmaller_Click);
+            // 
+            // btnrightnumber
+            // 
+            this.btnrightnumber.Location = new System.Drawing.Point(172, 131);
+            this.btnrightnumber.Name = "btnrightnumber";
+            this.btnrightnumber.Size = new System.Drawing.Size(75, 42);
+            this.btnrightnumber.TabIndex = 0;
+            this.btnrightnumber.Text = "Right Number";
+            this.btnrightnumber.UseVisualStyleBackColor = true;
+            this.btnrightnumber.Click += new System.EventHandler(this.btnrightnumber_Click);
+            // 
+            // btncheck
+            // 
+            this.btncheck.Location = new System.Drawing.Point(395, 131);
+            this.btncheck.Name = "btncheck";
+            this.btncheck.Size = new System.Drawing.Size(75, 42);
+            this.btncheck.TabIndex = 3;
+            this.btncheck.Text = "Check";
+            this.btncheck.UseVisualStyleBackColor = true;
+            this.btncheck.Click += new System.EventHandler(this.btncheck_Click);
+            // 
+            // lblAiQuestion
+            // 
+            this.lblAiQuestion.AutoSize = true;
+            this.lblAiQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAiQuestion.Location = new System.Drawing.Point(12, 88);
+            this.lblAiQuestion.Name = "lblAiQuestion";
+            this.lblAiQuestion.Size = new System.Drawing.Size(0, 20);
+            this.lblAiQuestion.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 256);
+            this.Controls.Add(this.btncheck);
+            this.Controls.Add(this.lblAiQuestion);
+            this.Controls.Add(this.lbAiSpeech);
+            this.Controls.Add(this.lblplayer);
+            this.Controls.Add(this.lblai);
+            this.Controls.Add(this.txtplayernum);
+            this.Controls.Add(this.btnsmaller);
+            this.Controls.Add(this.btnbigger);
+            this.Controls.Add(this.btnrightnumber);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Guess The Number";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtplayernum;
+        private System.Windows.Forms.Label lblai;
+        private System.Windows.Forms.Label lbAiSpeech;
+        private System.Windows.Forms.Label lblplayer;
+        private System.Windows.Forms.Button btnbigger;
+        private System.Windows.Forms.Button btnsmaller;
+        private System.Windows.Forms.Button btnrightnumber;
+        private System.Windows.Forms.Button btncheck;
+        private System.Windows.Forms.Label lblAiQuestion;
+    }
+}
+
---
+++ GuessTheNumber/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessTheNumber
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int ainum;
+        int i = 1;
+        int j = 100;
+        int mid =50;
+        bool playerturn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ManageAccess(false);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            ainum = rnd.Next(0, 101);
+            txtplayernum.Text = "";
+           //write with a variable mid
+
+            // button2.Click +=
+
+            //case mec e 
+            //function to change the color 
+            // case poqr e button 
+            // tommorow do hackerrank finally no lol fak u 
+            // 
+
+        }
+
+        public void TurnChange()
+        {
+            if (playerturn)
+            {
+                btnbigger.Enabled = false;
+                btnsmaller.Enabled = false;
+                btnrightnumber.Enabled = false;
+                btncheck.Enabled = true;
+                lblai.BackColor = Color.White;
+                lblplayer.BackColor = Color.FromArgb(192, 255, 192);
+                playerturn = false;
+            }
+            else
+            {
+                btnbigger.Enabled = true;
+                btnsmaller.Enabled =true;
+                btnrightnumber.Enabled = true;
+                btncheck.Enabled = false;
+                lblai.BackColor = Color.FromArgb(192, 255, 192);
+                lblplayer.BackColor = Color.White;
+                playerturn = true;
+
+            }
+            lblAiQuestion.Text = $"Is your number smaller or bigger than {mid}?";
+        }
+
+        public void ManageAccess(bool open)
+        {
+            btnbigger.Enabled = open;
+            btnsmaller.Enabled = open;
+            btnrightnumber.Enabled = open;
+            
+
+        }
+        private void btncheck_Click(object sender, EventArgs e)
+        {
+            int num = default;
+            
+            try
+            {
+                 num = Convert.ToInt32(txtplayernum.Text);
+
+            }
+            catch 
+            {
+                lbAiSpeech.Text = "Hey, you have to write a number !";
+                return;
+            }
+            ManageAccess(true);
+
+            //better check logic 
+            //check function
+            if (num > ainum)
+            {
+                lbAiSpeech.Text = "My number is smaller!";
+            }
+            else if (num < ainum)
+            {
+                lbAiSpeech.Text = "My number is bigger!";
+
+            }
+            else
+            {
+                lbAiSpeech.Text = "Wow, right number, congrats :)";
+                ManageAccess(false);
+
+            }
+            TurnChange();
+
+
+            //user thinks of a number
+        }
+     
+
+        private void btnbigger_Click(object sender, EventArgs e)
+        {
+
+            i = mid;
+            mid = (i + j) / 2;         
+            TurnChange();
+
+        }
+
+        private void btnsmaller_Click(object sender, EventArgs e)
+        {
+            j = mid;
+            mid = (i + j) / 2;
+            
+            TurnChange();
+
+        }
+
+        private void btnrightnumber_Click(object sender, EventArgs e)
+        {
+            lblAiQuestion.Text = "Yay, I won ! Try again ))";
+            ManageAccess(false);
+
+        }
+    }
+}
